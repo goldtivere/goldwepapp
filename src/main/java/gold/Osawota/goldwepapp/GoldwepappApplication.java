7e80ae1cfe37aa45ebff330098897b251e5621d5
goldtivere <goldtive@gmail.com>
2018-12-08 15:04:16 +0100

    first commit

---
+++ src/main/java/gold/Osawota/goldwepapp/GoldwepappApplication.java
@@ -0,0 +1,12 @@
+package gold.Osawota.goldwepapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldwepappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldwepappApplication.class, args);
+	}
+}
