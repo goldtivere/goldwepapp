5a8c559846ed5b8dd14a622d17c8fb9b994493b9
goldtivere <goldtive@gmail.com>
2018-12-09 20:19:00 +0100

    first commit

--- src/main/java/gold/Osawota/goldwepapp/model/Book.java
+++ src/main/java/gold/Osawota/goldwepapp/model/Book.java
@@ -12,10 +12,12 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    @JoinTable(name = "author_y", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors= new HashSet<>();
 
@@ -24,14 +26,14 @@ public class Book {
 
     }
 
-    public Book(String title,String isbn,String publisher)
+    public Book(String title, String isbn, Publisher publisher)
     {
 this.title=title;
 this.isbn=isbn;
 this.publisher=publisher;
     }
 
-    public Book(String title,String isbn,String publisher, Set<Author> authors)
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors)
     {
         this.title=title;
         this.isbn=isbn;
@@ -63,11 +65,11 @@ this.publisher=publisher;
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
